7c936a9866107ffe66631d1556434826fe045449
Pike <nghiavt1@arrowhitech.co>
2023-09-06 11:33:18 +0700

    initial

---
+++ app/Http/Controllers/Admin/Auth/LogoutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/admin/login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\User\Auth\LoginController;
+use App\Http\Controllers\User\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\Auth\LogoutController;
+
+Route::match(['get', 'post'], '/login', [LoginController::class, 'login'])->name('login');
+Route::middleware('auth')->group(function (){
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
+});
